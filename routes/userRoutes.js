513e16db4444e7f4a3977c3e4faad381ea888796
Austin-Cathey <austincatheyart@gmail.com>
2024-03-05 17:03:44 -0500

    updated endpoints and frontend adds user DB

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+
+/* router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/current", validateToken, currentUser); */
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,45 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require('cors');
+const users = require('./routes/userRoutes');
 
-const app = express();
+
 const PORT = 3000;
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect('mongodb+srv://austin:zlhHJ47JqHxraI1K@cluster0.eawefnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 const db = mongoose.connection;
 db.once("open", () => console.log("connected to mongoDB!!!!"));
-
+const User = require("./models/User");
 const HabitList = require("./models/HabitList");
-
+const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(morgan('dev'));
+app.use("/users", users);
+/* app.use(express.urlencoded({extended: true})); */ 
+
 
 //Get all DATA
 app.get("/everything", (req, res) => {
   HabitList.find().then((results) => res.status(200).json(results));
 });
-
+app.get("/user", (req, res) => {
+  User.find().then((results) => res.status(200).json(results));
+});
 //Add user 
-app.post("/user", (req, res) => {
+app.post("/signup", (req, res) => {
   const newHabitList = new HabitList(req.body);
   newHabitList.save();
   res.status(201).json(newHabitList);
 });
-
+//not working
+/* app.post("/newuser", (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save();
+  res.status(201).json(newUser);
+}); */
 //Add habit
 app.post("/user/:userId/habits", (req, res) => {
   HabitList.findById(req.params.userId)
